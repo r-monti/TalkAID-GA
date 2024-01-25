3262ed6685ace504cc6ab66cb4bd83e92c128080
Luigi <l.petrillo6@studenti.unisa.it>
2024-01-25 12:35:45 +0100

    Aggiutna multithreading per al selezioen degli esercizi ma per più utenti alla volta

--- GA/main.py
+++ GA/main.py
@@ -1,18 +1,31 @@
 from GA.Initialization.populationInitializer import initialize
-from GA.Selection.rouletteWheel import rouletteWheel as selection
+from GA.Selection.rankSelection import rankSelection as selection
 from GA.Crossover.crossover import executeCrossover as crossover
 import GA.Crossover.crossoverMethods as csType
 from GA.Mutation.mutationMethods import randomSingleMutation as mutation
+import threading
 
 p = initialize(4, 5, 904)
+p2 = initialize(4, 5, 914)
 
-while p.getGeneration() < 100:
-    print(f"Generation {p.getGeneration()}: P:{p}")
-    p.setIndividuals(selection(p))
-    p.setIndividuals(crossover(p, csType.nPoint, 2))
-    mutation(p, 0.5)
-    p.incrementGeneration()
+
+def task(*args):
+    print(f"Thread: {args[0]}")
+    print(f"Generation {args[1].getGeneration()}: P:{args[1]}")
+    args[1].setIndividuals(selection(args[1]))
+    args[1].setIndividuals(crossover(args[1], csType.nPoint, 2))
+    mutation(args[1], 0.5)
+    args[1].incrementGeneration()
     print("-----------------------------")
+
+
+while p.getGeneration() < 50:
+    thread1 = threading.Thread(target=task, args=(1, p))
+    thread2 = threading.Thread(target=task, args=(2, p2))
+    thread1.start()
+    thread2.start()
+    thread1.join()
+    thread2.join()
 
 
 print("\nResult:")
